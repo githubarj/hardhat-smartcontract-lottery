852616944fa606fec355b2a660ebf849d2432518
Richard Jeremy <githuba9520@gmail.com>
2022-09-27 17:45:14 +0300

    unit tests

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -5,7 +5,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("Raffle", async function () {
-          let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer;
+          let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer, interval;
           const chainId = network.config.chainId;
 
           beforeEach(async function () {
@@ -14,6 +14,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
               raffle = await ethers.getContract("Raffle", deployer);
               vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer);
               raffleEntranceFee = await raffle.getEntranceFee();
+              interval = await raffle.getInterval();
           });
 
           describe("contructor", async function () {
@@ -21,9 +22,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   // ideally one asset per it
 
                   //interval
-                  const interval = await raffle.getInterval();
                   assert.equal(interval.toString(), networkConfig[chainId]["interval"]);
-
                   // raffle state
                   const raffleState = await raffle.getRaffleState();
                   assert.equal(raffleState.toString(), "0");
@@ -47,6 +46,17 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.emit(
                       raffle,
                       "RaffleEnter"
+                  );
+              });
+
+              it("does not allow enter when raffle is calculating", async function () {
+                  await raffle.enterRaffle({ value: raffleEntranceFee });
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1]);
+                  await network.provider.send("evm_mine", []);
+                  //   we pretend to be chainlink keepers
+                  await raffle.performUpkeep([]);
+                  await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.be.revertedWith(
+                      "Raffle__NotOpen"
                   );
               });
           });
