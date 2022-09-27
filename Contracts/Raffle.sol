2b844f3c5d8427c1e454b4ba5fa3a35cc40a958c
Richard Jeremy <githuba9520@gmail.com>
2022-09-27 08:02:28 +0300

    modulo

--- Contracts/Raffle.sol
+++ Contracts/Raffle.sol
@@ -71,7 +71,10 @@ contract Raffle is VRFConsumerBaseV2 {
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
         internal
         override
-    {}
+    {
+        uint256 imdexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[imdexOfWinner];
+    }
 
     // view/pure functions
     function getEntranceFee() public view returns (uint256) {
